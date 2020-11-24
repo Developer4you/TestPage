370c592d313ac8f5897ad807408814b40766cfe0
Developer4you <avervalpet@gmail.com>
2020-11-24 21:26:28 +0300

    added elements and designed

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 const requestURL = 'https://jsonplaceholder.typicode.com/photos'
+let list = document.querySelector('.content')
+let bottonRequest = document.querySelector('.request')
+let bottonBackgroundBlue = document.querySelector('.background_blue')
+let header = document.querySelector('header')
+
 
 async function getResponse() {
     let response = await fetch(requestURL)
+
     let content = await response.json()
-    content = content.splice(0, 3)
+        content = content.splice(0, 5)
 
-    let list = document.querySelector('.content')
+        let key;
 
-    let key;
-
-    for (key in content) {
-        list.innerHTML += `
+        for (key in content) {
+            list.innerHTML += `
         <div class="post">
             <h4>${content[key].title}</h4>
-            <img src="${content[key].url}" width="150">
+            <img src="${content[key].url}" width="600">
             </div>
         `
-    }
-}
+        }
 
-getResponse();
+    }
+
+
+bottonRequest.addEventListener('click', event => {
+    getResponse()
+})
+
+/*bottonBackgroundBlue.addEventListener('click', event => {
+    list.setAttribute("style", "background-color: blue;")
+})*/
+
+function setColorBlue() {
+    list.setAttribute("style", "background-color: blue;")
+    }
+
